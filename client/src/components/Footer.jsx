07056675c7e720065628e84f84f8d797051adc78
PranayChavhan <pranay21chavhan@outlook.com>
2023-01-26 00:29:59 +0530

    link added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
     </Link>
 
     <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2023 AI ART —
-      <Link to="/" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@pranaychavhan</Link>
+      <Link to="https://www.linkedin.com/in/pranay-chavhan-38785a224/" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@pranaychavhan</Link>
     </p>
 
 
@@ -34,7 +34,7 @@ function Footer() {
       </Link>
 
 
-      <Link className="ml-3 text-gray-500">
+      <Link to="https://www.linkedin.com/in/pranay-chavhan-38785a224/" className="ml-3 text-gray-500">
         <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" className="w-5 h-5" viewBox="0 0 24 24">
           <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
           <circle cx="4" cy="4" r="2" stroke="none"></circle>
